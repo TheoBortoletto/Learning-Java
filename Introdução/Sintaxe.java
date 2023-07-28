060d8a1a527f89ea4a6ffb34d2160787803f1bcb
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-07-28 11:58:57 -0300

    add content

--- Introdução/Sintaxe.java
+++ Introdução/Sintaxe.java
@@ -1,6 +1,8 @@
 
 public class Sintaxe {
   public static void main(String[] args) {
-    System.out.println("\n\tHello World");
+
+    System.out.println("\n\tSintaxe Java: \n");
+    System.out.println("Hello World");
   }
 }
