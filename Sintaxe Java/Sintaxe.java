7491a9d9a11e9dbde66ffc1346da717e0977dcfa
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-07-28 12:06:15 -0300

    update readme & files

---
+++ Sintaxe Java/Sintaxe.java
@@ -0,0 +1,20 @@
+
+public class Sintaxe {
+  public static void main(String[] args) {
+
+    System.out.println("\n\tSintaxe Java: \n")
+    System.out.println("Hello World");
+  }
+}
+
+/*Exemplo explicado: 
+
+Cada linha de código executada em Java deve estar 
+dentro de um arquivo class. Em nosso exemplo, nomeamos 
+a classe Main . Uma classe deve sempre começar com uma 
+primeira letra maiúscula.
+
+Observação: Java diferencia maiúsculas de minúsculas: "MyClass" e "myclass" 
+têm significados diferentes.
+
+O nome do arquivo java deve corresponder ao nome da classe*/
